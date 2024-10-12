af59d7c55d506b37a080e9cae84c3915c7cbd13f
nel-brig1337 <nouhailaelbrighli09@gmail.com>
2024-10-12 16:21:02 +0100

    handle cases in ex01

--- Module00/ex01/Program.java
+++ Module00/ex01/Program.java
@@ -7,24 +7,33 @@ public class Program  {
         Scanner scanner = new Scanner(System.in);
         try {
             int inputNum = scanner.nextInt();
-            int steps = 0;
-            boolean found = true;
-            if (inputNum <= 0 || inputNum == 1) {
-                System.err.println("IllegalArgument");
-                return;
-            }
-            for (int i = 3; i <= Math.sqrt(inputNum); i++) {
-                steps++;
-                if (inputNum % i == 0) {
-                    found = false;
-                    break;
+            int steps = 1;
+            String msg = "";
+            if (inputNum <= 0 || inputNum == 1 || inputNum % 2 == 0) {
+                if (inputNum <= 0 || inputNum == 1) {
+                    msg = "IllegalArgument";
+                }
+                else if (inputNum == 2) {
+                    msg = "true 1";
+                }
+                else {
+                    msg = "false 1";
+                }
+            } else {
+                for (int i = 3; i <= inputNum / 2; i+= 2) {
+                    if (inputNum % i == 0) {
+                        msg = "false " + steps;
+                        break;
+                    }
+                    steps++;
                 }
             }
-            System.out.println(found + " " + steps);
+            if (msg.isEmpty()) {
+                msg = "true " + steps;
+            }
+            System.out.println(msg);
         } catch (Exception e) {
-            System.err.println("wrong number");
+            System.err.println("IllegalArgument");
         }
-    
-        
     }
 }
